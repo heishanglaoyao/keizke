8dea6a022fef1b9276d0e525da8e55a5e86a0954
黑上老妖 <oncwnuMBLzu2HjUMIrreitf7ASvc@git.weixin.qq.com>
2025-06-24 23:34:45 +0800

    dev:inti2

--- src/main/java/com/tencent/wxcloudrun/controller/GoodController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/GoodController.java
@@ -2,6 +2,7 @@ package com.tencent.wxcloudrun.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.model.GoodDto;
 import com.tencent.wxcloudrun.model.UserDto;
@@ -34,18 +35,17 @@ public class GoodController {
         return ApiResponse.ok(goodService.removeById(id));
     }
 
-    // 查询所有
-    @GetMapping("/list")
-    public ApiResponse<List<GoodDto>> list() {
-        return ApiResponse.ok(goodService.list());
-    }
 
     // 条件查询
-    @GetMapping("/query")
-    public ApiResponse<List<GoodDto>> query(@RequestParam(required = false) Integer categoryId) {
+    @GetMapping("/list")
+    public ApiResponse<List<GoodDto>> list(@RequestParam(required = false) Integer categoryId,
+                                           @RequestParam(required = false) String name) {
         LambdaQueryWrapper<GoodDto> queryWrapper = new LambdaQueryWrapper<>();
         if (categoryId != null) {
             queryWrapper.eq(GoodDto::getCategoryId, categoryId);
+        }
+        if(StringUtils.isNotBlank(name)){
+            queryWrapper.eq(GoodDto::getName, name);
         }
         return ApiResponse.ok(goodService.list(queryWrapper));
     }
