c8ffb144c8f21b88b14eaba6a25fa0d23209028e
黑上老妖 <oncwnuMBLzu2HjUMIrreitf7ASvc@git.weixin.qq.com>
2025-07-02 21:42:37 +0800

    dev:添加商品规格

--- src/main/java/com/tencent/wxcloudrun/service/impl/InOutTypeServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/InOutTypeServiceImpl.java
@@ -5,12 +5,13 @@ import com.tencent.wxcloudrun.dao.InOutTypeMapper;
 import com.tencent.wxcloudrun.model.InOutTypeDto;
 import com.tencent.wxcloudrun.service.InOutTypeService;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class InOutTypeServiceImpl extends ServiceImpl<InOutTypeMapper, InOutTypeDto> implements InOutTypeService {
 
     @Override
     public List<InOutTypeDto> listAllByName(String name) {
-        return List.of();
+        return Arrays.asList();
     }
 }
--- src/main/java/com/tencent/wxcloudrun/service/impl/StoreServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/StoreServiceImpl.java
@@ -5,12 +5,13 @@ import com.tencent.wxcloudrun.dao.StoreMapper;
 import com.tencent.wxcloudrun.model.StoreDto;
 import com.tencent.wxcloudrun.service.StoreService;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class StoreServiceImpl extends ServiceImpl<StoreMapper, StoreDto> implements StoreService {
 
     @Override
     public List<StoreDto> listAllByName(String name) {
-        return List.of();
+        return Arrays.asList();
     }
 }
